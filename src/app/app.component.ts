6bd2e34eb6f03278752388953689d3a36dd29c41
StYoukii <gcottin26@gmail.com>
2017-12-03 14:45:40 +0100

    Add server.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  events: Array<any>;
+
+  constructor(private _dataService: DataService) {
+    this._dataService.getEvents()
+      .subscribe(res => this.events = res);
+  }
+
+  title = 'Hyge';
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DataService {
+
+  result: any;
+
+  constructor(private _http: Http) {
+
+  }
+
+  // Get the list of all events
+  getEvents() {
+    return this._http.get("/events")
+      .map(result => this.result = result.json().data);
+  }
+
+  // Get the list of all users
+  getUsers() {
+    return this._http.get("/users")
+      .map(result => this.result = result.json().data);
+  }
+
+}
